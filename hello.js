10f95d5bd153754c690bd522668aad6a0a8500fc
paulmatthews198 <>
2021-05-03 19:26:26 +0000

    attempt 1

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello!";
+	return "Hello there general kenobi!";
 }
  
 module.exports = hello;
